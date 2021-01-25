b4738d0cb27112fffcf6c484e7f035c05d298b6e
Pratik <pednekar.pratik10@gmail.com>
2021-01-25 16:48:00 +1100

    phase 1 commit

--- sample-project-np/src/components/Button.js
+++ sample-project-np/src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
-import signup from './auth/SignUp';
+import signup from './auth/SignUpAuth';
 
 export function Button() {
     return (
